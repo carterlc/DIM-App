c3237e4cc9d7108063d3cd4a9585c6055ab50fce
Carter Clifford <carterlc@live.ca>
2023-03-21 14:02:51 -0600

    Function and prop renaming to be more fit within my destiny2 item manager app

--- App.js
+++ App.js
@@ -4,42 +4,42 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function DetailsScreen() {
+function SettingsScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', backgroundColor: '#202020' }}>
-      <Text style={{color: 'white', fontSize: 20, marginBottom: 20, marginTop: 10}}>How to use DIM (Destiny Item Manager)</Text>
-      <Text style={{color: 'white'}}>1. Sign in using you Bungie name and information</Text>
-      <Text style={{color: 'white'}}>2. Navigate to your Vault. It should be on the second tab.</Text>
-      <Text style={{color: 'white'}}>3. </Text>
+      <Text style={{ color: 'white', fontSize: 20, marginBottom: 20, marginTop: 10 }}>How to use DIM (Destiny Item Manager)</Text>
+      <Text style={{ color: 'white' }}>1. Sign in using you Bungie name and information</Text>
+      <Text style={{ color: 'white' }}>2. Navigate to your Vault. It should be on the second tab.</Text>
+      <Text style={{ color: 'white' }}>3. </Text>
     </View>
   );
 }
 
-function HomeScreen({ navigation }) {
+function CharacterScreen({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#202020' }}>
-      <Pressable style={styles.settings} onPress={() => navigation.navigate('Details')}>
+      <Pressable style={styles.settings} onPress={() => navigation.navigate('Settings')}>
         <Image source={require('./assets/icons8-settings-24.png')} />
       </Pressable>
     </View>
   );
 }
 
-function SettingsScreen({ navigation }) {
+function VaultScreen({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#202020' }}>
-      <Pressable style={styles.settings} onPress={() => navigation.navigate('Details')}>
+      <Pressable style={styles.settings} onPress={() => navigation.navigate('Settings')}>
         <Image source={require('./assets/icons8-settings-24.png')} />
       </Pressable>
     </View>
   );
 }
 
-const HomeStack = createNativeStackNavigator();
+const CharacterStack = createNativeStackNavigator();
 
-function HomeStackScreen() {
+function CharacterStackScreen() {
   return (
-    <HomeStack.Navigator
+    <CharacterStack.Navigator
       screenOptions={{
         headerStyle: {
           backgroundColor: 'black',
@@ -49,17 +49,17 @@ function HomeStackScreen() {
           fontWeight: 'bold',
         },
       }}>
-      <HomeStack.Screen name="Home" component={HomeScreen} />
-      <HomeStack.Screen name="Details" component={DetailsScreen} />
-    </HomeStack.Navigator>
+      <CharacterStack.Screen name="Character" component={CharacterScreen} />
+      <CharacterStack.Screen name="Settings" component={SettingsScreen} />
+    </CharacterStack.Navigator>
   );
 }
 
-const SettingsStack = createNativeStackNavigator();
+const VaultStack = createNativeStackNavigator();
 
-function SettingsStackScreen() {
+function VaultStackScreen() {
   return (
-    <SettingsStack.Navigator
+    <VaultStack.Navigator
       screenOptions={{
         headerStyle: {
           backgroundColor: 'black',
@@ -69,9 +69,9 @@ function SettingsStackScreen() {
           fontWeight: 'bold',
         },
       }}>
-      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
-      <SettingsStack.Screen name="Details" component={DetailsScreen} />
-    </SettingsStack.Navigator>
+      <VaultStack.Screen name="Vault" component={VaultScreen} />
+      <VaultStack.Screen name="Settings" component={SettingsScreen} />
+    </VaultStack.Navigator>
   );
 }
 
@@ -98,9 +98,18 @@ export default function App() {
           height: 45,
         },
       }}
+        tabBarOptions={{
+          activeTintColor: 'orange',
+          inactiveTintColor: 'gray',
+          labelStyle: {
+            fontSize: 16,
+            fontWeight: 'bold',
+          },
+        }}
+
       >
-        <Tab.Screen name="Home" component={HomeStackScreen} />
-        <Tab.Screen name="Settings" component={SettingsStackScreen} />
+        <Tab.Screen name="Character" component={CharacterStackScreen} />
+        <Tab.Screen name="Vault" component={VaultStackScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
