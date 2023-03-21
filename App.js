319ca303da27ad52e724f0f935edd448a13cb7d2
Carter Clifford <carterlc@live.ca>
2023-03-21 13:46:44 -0600

    Added some destiny 2 armor/weapon png so I can fill up the vault.

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
 import * as React from 'react';
-import { Button, Text, View, BlurView, Pressable, Image } from 'react-native';
+import { Button, Text, View, BlurView, Pressable, Image, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 function DetailsScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Details!</Text>
+    <View style={{ flex: 1, alignItems: 'center', backgroundColor: '#202020' }}>
+      <Text style={{color: 'white', fontSize: 20, marginBottom: 20, marginTop: 10}}>How to use DIM (Destiny Item Manager)</Text>
+      <Text style={{color: 'white'}}>1. Sign in using you Bungie name and information</Text>
+      <Text style={{color: 'white'}}>2. Navigate to your Vault. It should be on the second tab.</Text>
+      <Text style={{color: 'white'}}>3. </Text>
     </View>
   );
 }
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Pressable onPress={() => navigation.navigate('Details')}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#202020' }}>
+      <Pressable style={styles.settings} onPress={() => navigation.navigate('Details')}>
         <Image source={require('./assets/icons8-settings-24.png')} />
       </Pressable>
     </View>
@@ -24,8 +27,8 @@ function HomeScreen({ navigation }) {
 
 function SettingsScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Pressable onPress={() => navigation.navigate('Details')}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#202020' }}>
+      <Pressable style={styles.settings} onPress={() => navigation.navigate('Details')}>
         <Image source={require('./assets/icons8-settings-24.png')} />
       </Pressable>
     </View>
@@ -36,7 +39,16 @@ const HomeStack = createNativeStackNavigator();
 
 function HomeStackScreen() {
   return (
-    <HomeStack.Navigator>
+    <HomeStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: 'black',
+        },
+        headerTintColor: 'white', // Change font color to white
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
       <HomeStack.Screen name="Home" component={HomeScreen} />
       <HomeStack.Screen name="Details" component={DetailsScreen} />
     </HomeStack.Navigator>
@@ -47,7 +59,16 @@ const SettingsStack = createNativeStackNavigator();
 
 function SettingsStackScreen() {
   return (
-    <SettingsStack.Navigator>
+    <SettingsStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: 'black',
+        },
+        headerTintColor: 'white', // Change font color to white
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
       <SettingsStack.Screen name="Settings" component={SettingsScreen} />
       <SettingsStack.Screen name="Details" component={DetailsScreen} />
     </SettingsStack.Navigator>
@@ -56,38 +77,31 @@ function SettingsStackScreen() {
 
 const Tab = createBottomTabNavigator();
 
+const styles = StyleSheet.create({
+  settings: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+  },
+  plainText: {
+    backgroundColor: 'white'
+  }
+})
+
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer >
       <Tab.Navigator screenOptions={{
         headerShown: false,
         tabBarStyle: {
-          backgroundColor: 'purple',
-          height: 50,
-          activeTintColor: 'pink',
+          backgroundColor: 'black',
+          height: 45,
         },
-        tabBarItemStyle: {
-          backgroundColor: '#B65FCF',
-          margin: 5,
-          borderRadius: 10,
-          activeTintColor: 'pink',
-        }
       }}
-        TabBarOptions={{
-          activeTintColor: 'pink',
-        }}
-        renderLabel={{
-          color: 'white',
-        }}
       >
-        <Tab.Screen name="Home" component={HomeStackScreen} options={{ tabBarBadge: 3 }} />
-        <Tab.Screen name="Settings" component={SettingsStackScreen} options={{ tabBarBadge: 999 }} />
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Settings" component={SettingsStackScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
-
-  const styles = StyleSheet.create({
-
-  })
-
 }
